74af4eb8ae0e16b850dce2a648c3702795ad0d6c
Samani Mukhtar <samani@Samanis-MacBook-Pro.local>
2023-03-11 08:33:53 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class Thermometer:
+    currentTemp = 23
+
+    def __init__(self, freezing, boiling):
+        self.freezing_threshold = freezing
+        self.boiling_threshold = boiling
+
+    def __str__(self):
+        return str(self.currentTemp)
+
+
+t1 = Thermometer(0, 100)
+print(t1)
